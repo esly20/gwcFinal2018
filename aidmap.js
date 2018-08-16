13d38864725d731e6c9c366a0e7443ee8c47b732
alexandraabrego1 <39199730+alexandraabrego1@users.noreply.github.com>
2018-08-16 15:05:06 -0400

    Update aidmap.js

--- aidmap.js
+++ aidmap.js
@@ -1,39 +1,102 @@
-// This is for all the map functions
-var mymap = L.map('mapid').setView([40.720530, -73.997338], 12);
+<!DOCTYPE>
+<html>
+  <head>
+    <link rel ="stylesheet" href="finalproject.css">
+
+    <!-- The following should NOT be deleted. Needed for map -->
+    <style>
+    #mapid {height: 500px; width: 900px}
+    </style>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.3/dist/leaflet.css"
+      integrity="sha512-Rksm5RenBEKSKFjgI3a41vrjkw4EVPlJ3+OiI65vTjIdo9brlAacEuKOiQ5OFh7cOI1bkDwLqdLw3Zg0cRJAAQ=="
+      crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.3.3/dist/leaflet.js"
+      integrity="sha512-tAGcCfR4Sc5ZP5ZoVz0quoZDYX5aCtEm/eu1KhSLj2c9eFrylXZknQYmxUssFaVJKvvc0dJQixhGjG2yXWiV9Q=="
+      crossorigin=""></script>
+
+  </head>
+<body>
+    <div id="google_translate_element"></div>
+<script>
+    function googleTranslateElementInit() {
+        new google.translate.TranslateElement(
+            {pageLanguage: 'en'},
+            'google_translate_element'
+        );
+    }
+</script>
+<script src="element.js"></script>
+    <div class='header'>
+          <h1>Aid</h1>
+        </div>
+          <div class='navbar'>
+          <a href='mainpage.html'>Home</a>
+          <a href='getinvolved.html'>Get involved</a>
+          <a href='learnmore.html'>Learn More</a>
+     </div>
+     <center><h2>Homeless Shelters and Food Banks in NYC</h2></center>
+     <div class='body'>
+
+       <div class='row'>
+         <div class='column2'>
+           <center>
+
+
+             <a href='http://nycrescue.org/' target="_blank"><strong>New York City Rescue Mission</strong></a>
+             <p>90 Lafayette St, New York, NY 10013<p>
+             <p>Open 24 Hours<p>
+             <a href="http://www.projectrenewal.org/program-list/2013/3/6/fort-washington-mens-shelter"  target="_blank"><strong>Fort Washington's Men Shelter</strong></a>
+             <p>651 W 168th St, New York, NY 10032<p>
+             <a href='https://ny.covenanthouse.org/'  target="_blank"><strong>Covenant House New York</strong></a>
+             <p>460 W 41st St, New York, NY 10036<p>
+             <p>Open 24 Hours</p>
+             <a href='http://www.iclinc.net/search-results/tillary-street-womens-shelter/'  target="_blank"><strong>Tillary Street Women's Shelter</strong></a>
+             <p>200 Tillary St, Brooklyn, NY 11201<p>
+             <a href='http://www1.nyc.gov/'  target="_blank"><strong>HRA Men's Shelter</strong></a>
+             <p>400 E 30th St, New York, NY 10016<p>
+             <p>Open 24 Hours<p>
+             <a href='http://www.lenoxhill.org/wmhsparkavearmory/'  target="_blank"><strong>Lenox Hill Neighborhood House Women</strong></a>
+             <p>643 Park Ave, New York, NY 10065<p>
+             <a href='http://www.coalitionforthehomeless.org/'  target="_blank"><strong>Coalition for the Homeless</strong></a>
+             <p>129 Fulton St, New York, NY 10038<p>
+            </div>
 
 
 
-function show_map(mymap){
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-      maxZoom: 19,
-      id: 'mapbox.satellite', //satellite for earth
-      accessToken: 'sk.eyJ1IjoiZXNsaWJlY2siLCJhIjoiY2prdjd1Y2NwMDBsMjNwcWs5MHViamlxOCJ9.7sLzqelXVl35Lomwxsro1g'
-  }).addTo(mymap);
-}
-
-function add_marker(mymap){
-  //nycrm.bindPopup("<b>New York City Rescure Mission</b><br>Christy is dumb.");
-  var nycrm = L.marker([40.717542, -74.001615]).addTo(mymap);
-  var fortwash = L.marker([40.841451, -73.940417]).addTo(mymap);
-  var covenant = L.marker([40.758550, -73.995576]).addTo(mymap);
-  var tillary = L.marker([40.695731, -73.982145]).addTo(mymap);
-  var hra = L.marker([40.740655, -73.974382]).addTo(mymap);
-  var lenox = L.marker([40.767076, -73.957739]).addTo(mymap);
-  var lenox2 = L.marker([40.767233, -73.965504]).addTo(mymap);
-  nycrm.bindPopup("<b>New York City Rescue Mission</b><br><a href='https://www.google.com/maps/dir//New+York+City+Rescue+Mission,+Lafayette+Street,+New+York,+NY/@40.7297865,-73.9805614,16z/data=!4m9!4m8!1m0!1m5!1m1!1s0x89c25a20ba92b777:0xb97c04f0980ebdbb!2m2!1d-74.0016513!2d40.7174328!3e0' target='blank'>Get Directions</a>");
-  fortwash.bindPopup("<b>Fort Washington Men's Shelter</b><br><a href='https://www.google.com/maps/dir/40.7310106,-73.9776983/Fort+Washington+Men's+Shelter,+West+168th+Street,+New+York,+NY/@40.7895777,-73.9536577,12z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x89c2f69bf798778b:0xb5091938d4c9630c!2m2!1d-73.941217!2d40.8418743!3e0' target='blank'>Get Directions</a>");
-  covenant.bindPopup("<b>Covenant House New York</b><br><a href='https://www.google.com/maps/dir/40.7310106,-73.9776983/Covenant+House+New+York,+West+41st+Street,+New+York,+NY/@40.7436585,-73.9870647,14z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x89c2584d6fd84f13:0xaf16f778cf8e9b35!2m2!1d-73.995957!2d40.758501!3e0' target='blank'>Get Directions</a>");
-  tillary.bindPopup("<b>Tillary Street Women's Center</b><br><a href='https://www.google.com/maps/dir//Tillary+Street+Women%E2%80%99s+Shelter,+Tillary+Street,+Brooklyn,+NY/@40.695566,-73.9820974,12z/data=!4m8!4m7!1m0!1m5!1m1!1s0x89c25bcaa7bd5587:0x8a7be7297f332f7!2m2!1d-73.9820974!2d40.695566' target='blank'>Get Directions</a>");
-  hra.bindPopup("<b>HRA Men's Shelter</b><br><a href='https://www.google.com/maps/dir//400+E+30th+St,+New+York,+NY+10016/@40.740577,-73.9769597,17z/data=!4m8!4m7!1m0!1m5!1m1!1s0x89c2590ef236310d:0xf9fd636671cdb359!2m2!1d-73.974771!2d40.740577' target='blank'>Get Directions</a>");
-  lenox.bindPopup("<b>Lenox Hill Neighborhood House for Women</b></a><br><a href='https://www.google.com/maps/dir/40.7310106,-73.9776983/Lenox+Hill+Neighborhood+House+Women%E2%80%99s+Mental+Health+Shelter,+643+Park+Ave,+New+York,+NY+10065/@40.7480078,-73.9878899,14z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x89c258ec1d12bdf9:0xddec6fbd41df4169!2m2!1d-73.9653575!2d40.7671771!3e0' target='blank'>Get Directions</a>");
-  lenox2.bindPopup("<b>Lenox Hill Women's Mental Health Shelter</b><br><a href='https://www.google.com/maps/dir/40.7310106,-73.9776983/Lenox+Hill+Neighborhood+House+Women%E2%80%99s+Mental+Health+Shelter,+Park+Avenue,+New+York,+NY/@40.7480092,-73.9703803,13z/data=!3m1!4b1!4m10!4m9!1m1!4e1!1m5!1m1!1s0x89c258ec1d12bdf9:0xddec6fbd41df4169!2m2!1d-73.9653575!2d40.7671771!3e0' target='blank'>Get Directions</a>");
-}
 
 
 
-function onMapClick() {
-    alert("Click and drag on the map to move around, and scroll in and out to zoom. Select locations to get directions.");
-}
-show_map(mymap)
-add_marker(mymap)
+
+
+
+          </center>
+            <div class='column3'>
+              <center>
+              <a href='https://www1.nyc.gov/'  target="_blank"><strong>Family Shelters Bronx</strong></a>
+              <p>151 E 151st St, Bronx, NY 10454<p>
+              <P>Open 24 Hours<p>
+              <a href='http://stanthonyshelter.org/'  target="_blank"><strong>St.Anthony Shelter for Renewal</strong></a>
+              <p>410 E 156th St, Bronx, NY 10455<p>
+              <a href='https://www.safehorizon.org/'  target="_blank"><strong>Safe Horizon</strong></a>
+              <p> 189 Montague St, Brooklyn, NY 11201<p>
+              <a href='http://www.partnershipforthehomeless.org/'  target="_blank"><strong>Parterniship for the Homeless</strong></a>
+              <p>305 7th Ave 14 fl, New York, NY 10001<p>
+              <a href='https://www.cityharvest.org/'  target="_blank"><strong>City Harvest</strong></a>
+              <p>6 E 32nd St, New York, NY 10016<p>
+
+
+              </center>
+            </div>
+          </div>
+
+      </div>
+
+
+    <center>
+     <!-- For Map -->
+     <div id="mapid"></div>
+     <script src="mapfunctions.js"></script>
+    </center>
+
+    </body>
+</html>
